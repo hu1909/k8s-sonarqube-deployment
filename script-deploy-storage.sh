188c2bef0c67670f55d4e5dd1f45566bb2b121b0
vagrant <vagrant@control-plane>
2025-06-24 09:23:37 +0000

    A little change in script

--- script-deploy-storage.sh
+++ script-deploy-storage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 
-$VARIABLE_PATH="./variable-storage"
-$SERVICE_PATH="./service-definition"
+$VARIABLE_PATH="./variable-storage/"
+$SERVICE_PATH="./service-definition/"
 $DEPLOYMENT_PATH="./deployment-definition/"
 $PERSISTENT_PATH="./persistent_volume/"
 $PERSISTENT_CLAIM_PATH="./persistent_volume_claim/"
 
 auto_deploy() {
     find $1 -type f | while read -r file; do 
-        
+         
         echo "Processing different file: $file"
         kubectl apply -f $file
     done
